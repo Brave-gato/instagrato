427667081c14b31a91218b01b6850014b7f1ccda
Brave-gato <webrepoleved@google.com>
2024-10-16 16:08:47 +0200

    changement Post, User, Post Factoriy, migration

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,11 +1,15 @@
+
 <?php
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
  */
 class PostFactory extends Factory
 {
@@ -17,7 +21,18 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+
+            'caption' => fake()->realTextBetween($minNbChars = 500, $maxNbChars = 2000),
+            'img_path' => function () {
+                $randomName = Str::uuid();
+                $imageUrl = "https://picsum.photos/1024/768.webp?random={$randomName}";
+                $path = "images/{$randomName}.webp";
+                Storage::disk('public')->put($path, file_get_contents($imageUrl));
+
+                return $path;
+            },
+
+            'user_id' => User::get()->random()->id,
         ];
     }
 }
