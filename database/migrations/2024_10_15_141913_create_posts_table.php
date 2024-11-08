eff50ca9122228dfe679e17d5dea53dcc5550db9
Brave-gato <webrepoleved@google.com>
2024-11-08 19:45:42 +0100

    création de controller User, Post Comment, Like. Correction Migration Posts_tables

--- database/migrations/2024_10_15_141913_create_posts_table.php
+++ database/migrations/2024_10_15_141913_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('img_path');
             $table->text('caption')->nullable();
-            $table->timestamps('createAt');
+            $table->timestamps();
         });
     }
 
