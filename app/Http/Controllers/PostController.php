78fb870c5138634cb45d2298b4b8da3772a6081a
Brave-gato <webrepoleved@google.com>
2024-11-08 20:38:39 +0100

    modif controllers User, Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,4 +7,38 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'caption' => 'nullable|string|max:1000',
+            'image' => 'required|image|max:2048',
+        ]);
+
+        $imagePath = $request->file('image')->store('posts', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'img_path' => $imagePath,
+        ]);
+
+        return redirect()->route('home')->with('success', 'Post created successfully');
+    }
+
+    public function index()
+    {
+        $followedUsers = auth()->user()->following()->pluck('id');
+        $posts = Post::whereIn('user_id', $followedUsers)
+            ->orWhereHas('likes', '>', 10)
+            ->latest()
+            ->paginate(15);
+
+        return view('home', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function edit()
+    {
+        return view('users.edit', ['user' => auth()->user()]);
+    }
+
+    public function update(Request $request)
+    {
+        $user = auth()->user();
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'bio' => 'nullable|string|max:1000',
+            'profile_photo' => 'nullable|image|max:1024',
+        ]);
+
+        if ($request->hasFile('profile_photo')) {
+            $path = $request->file('profile_photo')->store('profile_photos', 'public');
+            $data['profile_photo'] = $path;
+        }
+
+        $user->update($data);
+
+        return redirect()->route('profile.show', $user)->with('success', 'Profile updated successfully');
+    }
+
+    public function follow(User $user)
+    {
+        auth()->user()->following()->attach($user->id);
+        return back()->with('success', 'You are now following ' . $user->name);
+    }
+
+    public function unfollow(User $user)
+    {
+        auth()->user()->following()->detach($user->id);
+        return back()->with('success', 'You have unfollowed ' . $user->name);
+    }
+
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $users = User::where('name', 'like', "%$query%")->get();
+        $posts = Post::where('caption', 'like', "%$query%")->get();
+
+        return view('search.results', compact('users', 'posts'));
+    }
+
+
+    public function show(User $user)
+    {
+        $posts = $user->posts()->latest()->paginate(12);
+        return view('users.show', compact('user', 'posts'));
+    }
+
 }
